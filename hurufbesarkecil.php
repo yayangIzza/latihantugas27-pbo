ddbac9baab92bf1e2c0b55c269a27826a7a317c5
yayangIzza <149374422+yayangIzza@users.noreply.github.com>
2023-10-30 19:55:20 +0700

    Add files via upload

---
+++ hurufbesarkecil.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "Nama  : Yayang Izzatul Haq<br>";
+echo "NIM   : 22205052<br>";
+echo "Kelas : PBO<br>";
+
+
+echo "Masukkan kalimat : apA aKu dI MArs?<br>";
+
+echo "=====Hail Formatting=====<br>";
+echo "HURUF BESAR : APA AKU DI MARS?<br>";
+echo "huruf kecil : aku aku di mars?<br>";
+
+?>
